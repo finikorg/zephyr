8eae52f8a9211b008d2585e8c96029d333e26c7e
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2021-02-10 16:52:19 +0200

    hack: update usermode
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- samples/subsys/edac/src/main.c
+++ samples/subsys/edac/src/main.c
@@ -9,28 +9,80 @@
 #include <devicetree.h>
 
 #include <drivers/edac.h>
+#include <ibecc.h>
+
+#include <app_memory/app_memdomain.h>
 
 #include <logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
-#define STACKSIZE	1024
-#define PRIORITY	7
+#define STACK_SIZE 1024
+#define PRIORITY 7
+K_THREAD_STACK_DEFINE(thread_stack, STACK_SIZE);
+struct k_thread thread_id;
 
-static atomic_t handled;
+K_APPMEM_PARTITION_DEFINE(default_part);
+K_APP_BMEM(default_part) static atomic_t handled;
+K_APP_BMEM(default_part) static volatile uint32_t error_type;
+K_APP_BMEM(default_part) static volatile uint64_t error_address;
+K_APP_BMEM(default_part) static volatile uint16_t error_syndrome;
+
+#if 0
+/* Initialize statically message queue */
+#define MAX_MSGS 4
+K_MSGQ_DEFINE(mqueue, sizeof(uint8_t), MAX_MSGS, 4);
+#endif
 
 /**
  * Callback called from ISR. Runs in supervisor mode
  */
 static void notification_callback(const struct device *dev, void *data)
 {
+	struct ibecc_error *error_data = data;
+
 	/* Special care need to be taken for NMI callback:
 	 * delayed_work, mutex and semaphores are not working stable
 	 * here, using integer increment for now
 	 */
+
+	error_type = error_data->type;
+	error_address = error_data->address;
+	error_syndrome = error_data->syndrome;
+
 	atomic_set(&handled, true);
+#if 0
+	int ret;
+	ret = k_msgq_put(&mqueue, &test, K_NO_WAIT);
+	if (ret) {
+		LOG_ERR("k_msgq_put failed with %d", ret);
+	}
+#endif
 }
 
 #define DEVICE_NAME DT_LABEL(DT_NODELABEL(ibecc))
+
+void thread_function(void *p1, void *p2, void *p3)
+{
+	LOG_INF("Thread started");
+#if 1
+	while (true) {
+		if (atomic_cas(&handled, true, false)) {
+			/* make nice printing */
+			k_sleep(K_MSEC(300));
+			printk("\nGot notification about IBECC event\n");
+			printk("Parameters: %d %llx %x\n",
+			       error_type, error_address, error_syndrome);
+		}
+
+		k_sleep(K_MSEC(300));
+	}
+#else
+	while (true) {
+		k_msgq_get(&mqueue, payload, K_FOREVER);
+		LOG_INF("Got notification");
+	}
+#endif
+}
 
 void main(void)
 {
@@ -48,19 +100,13 @@ void main(void)
 	}
 
 	LOG_INF("EDAC shell application initialized");
+
+	k_mem_domain_add_partition(&k_mem_domain_default, &default_part);
+
+	k_thread_create(&thread_id, thread_stack, STACK_SIZE, thread_function,
+			NULL, NULL, NULL, PRIORITY, K_USER, K_FOREVER);
+#if 0
+	//k_thread_access_grant(&thread_id, &mqueue);
+#endif
+	k_thread_start(&thread_id);
 }
-
-void thread_function(void)
-{
-	LOG_DBG("Thread started");
-
-	while (true) {
-		if (atomic_cas(&handled, true, false)) {
-			printk("Got notification about IBECC event\n");
-			k_sleep(K_MSEC(300));
-		}
-	}
-}
-
-K_THREAD_DEFINE(thread_id, STACKSIZE, thread_function, NULL, NULL, NULL,
-		PRIORITY, 0, 0);
