6f46e49aa91ab8ceeaa5cd7e0863442c56008427
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2021-02-12 18:31:22 +0200

    hack:
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- tests/arch/x86/nmi/src/main.c
+++ tests/arch/x86/nmi/src/main.c
@@ -35,6 +35,14 @@ uint8_t *nmi_stacks[] = {
 #endif
 };
 
+#define MAX_MSGS 4
+K_MSGQ_DEFINE(mqueue, sizeof(int), MAX_MSGS, 4);
+
+#define STACK_SIZE 1024
+#define PRIORITY 7
+K_THREAD_STACK_DEFINE(thread_stack, STACK_SIZE);
+struct k_thread thread_id;
+
 bool z_x86_do_kernel_nmi(const z_arch_esf_t *esf)
 {
 	uint64_t stack;
@@ -49,6 +57,8 @@ bool z_x86_do_kernel_nmi(const z_arch_esf_t *esf)
 		     CONFIG_X86_EXCEPTION_STACK_SIZE, "Incorrect stack");
 
 	int_handler_executed++;
+
+	k_msgq_put(&mqueue, (void *)&int_handler_executed, K_NO_WAIT);
 
 	return true;
 }
@@ -66,8 +76,34 @@ void test_nmi_handler(void)
 		      int_handler_executed);
 }
 
+void thread_function(void *p1, void *p2, void *p3)
+{
+	TC_PRINT("Thread started");
+
+	while (true) {
+		int payload;
+
+		k_msgq_get(&mqueue, &payload, K_FOREVER);
+		printk("Got notification, data %d", payload);
+	}
+}
+
+void test_nmi_handler_user(void)
+{
+	k_thread_create(&thread_id, thread_stack, STACK_SIZE, thread_function,
+			NULL, NULL, NULL, PRIORITY, K_USER, K_FOREVER);
+	k_thread_access_grant(&thread_id, &mqueue);
+	k_thread_start(&thread_id);
+
+	for (int i = 0; i < 20; i++) {
+		//k_sleep(K_MSEC(100));
+		_trigger_isr_handler(IV_NON_MASKABLE_INTERRUPT);
+	}
+}
+
 void test_main(void)
 {
-	ztest_test_suite(nmi, ztest_unit_test(test_nmi_handler));
+	ztest_test_suite(nmi, ztest_unit_test(test_nmi_handler),
+			 ztest_unit_test(test_nmi_handler_user));
 	ztest_run_test_suite(nmi);
 }
